ccb080240c0b58333d96be75066ec38ed2ee0f6e
Navneet Singh <navneetsingh1825@gmail.com>
2025-02-25 02:20:35 +0530

    Updated App.jsx to ensure proper navigation between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes ,useLocation} from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Delivery from "./Pages/Delivery/Delivery";
+import PrivacyPolicy from "./Pages/PrivacyPolicy/PrivacyPolicy";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
   const [showLogin,setShowLogin] = useState(false)
+  const location = useLocation(); 
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <>
     {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
@@ -16,6 +24,9 @@ const App = () => {
         <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
         </Routes>
